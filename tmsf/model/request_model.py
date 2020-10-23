d0a9df3337e46dbd5d21395d7cb6d40ce7fa96fa
隼耗 <sunhao1109@outlook.com>
2020-10-23 17:58:15 +0800

    爬取透明售房网中和雅轩剩余房源数

--- tmsf/model/request_model.py
+++ tmsf/model/request_model.py
@@ -5,7 +5,7 @@ class RequestModel:
 
     def __init__(self):
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            "User-Agent": HTTP_HEADER.USER_AGENT,
             "Host": "www.tmsf.com",
             "Cookie": "gr_user_id=3005213d-5479-48c0-b0f8-434c1f87ec87; grwng_uid=e4539a4c-bd21-4912-b000-f03aca4f9a22; bdshare_firstime=1573040595262; Hm_lvt_18f10d8299bec7ffdf82d66d10c93213=1574051412; Hm_lvt_5bbc90d068807d82c72909ecd298e11a=1587905233; UM_distinctid=17206b6bc1f82f-0a73c4cc5c1cd5-d373666-1fa400-17206b6bc209a5; br_access_code=Y+sLp1s+6NsAAAAAvJUaXwAAAABByJsM; X-BR-AntiSpider-Status=1; Hm_lvt_bbb8b9db5fbc7576fd868d7931c80ee1=1603069895,1603173095,1603244216,1603337100; b61f24991053b634_gr_session_id=bbf22a0d-33b3-47f2-b7a2-7b8c2a7edd10; b61f24991053b634_gr_session_id_bbf22a0d-33b3-47f2-b7a2-7b8c2a7edd10=true; BSFIT_EXPIRATION=1603483087805; BSFIT_DEVICEID=e9yTRO-BJtyYdGI0kEJjEyxQcHZSjs0MSX2E0-c4y6FTnoB4cXCsfruvGAoB6QuzRQWHfpG0OdgvIXTFRNtnxntgKiCQSLfkmq3o9jQG84FSGjnIjpAGVYYmkcuam06BsQBopx3sUkz7LVS-SQZOxmMK-Vbq_3zD; JSESSIONID=0400BDAD7BD2AE4D76DFBC1949327D97; CNZZDATA1253675216=1241644750-1576396375-%7C1603436065; __qc_wId=713; pgv_pvid=2240125425; BSFIT_k1w2l=; Hm_lpvt_bbb8b9db5fbc7576fd868d7931c80ee1=1603437865",
         }
